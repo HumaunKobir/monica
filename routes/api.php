1672cc576818217154d9f93a34574825451a77d5
HumaunKobir <hkitune6@gmail.com>
2026-06-05 14:31:54 +0600

    create user login api token route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Domains\Settings\ManageUsers\Api\Controllers\UserController;
 use App\Domains\Vault\ManageVault\Api\Controllers\TagApiController;
 use App\Domains\Vault\ManageVault\Api\Controllers\VaultController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +16,23 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::get('/get-token', function () {
+    $user = User::where('email', 'hkitune6@gmail.com')->first();
 
+    if (! $user) {
+        return response()->json(['error' => 'User not found'], 404);
+    }
+
+    $user->tokens()->delete();
+
+    $token = $user->createToken('api-token')->plainTextToken;
+
+    return response()->json([
+        'token' => $token,
+        'user_id' => $user->id,
+        'email' => $user->email,
+    ]);
+});
 Route::middleware('auth:sanctum')->name('api.')->group(function () {
     // users
     Route::get('user', [UserController::class, 'user']);
